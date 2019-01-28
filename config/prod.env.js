4e2b3dd77277b73cc143e9ae76d62c92aaa64c08
yanghoon <ssang276@hanmail.net>
2019-01-28 10:51:13 +0900

    ..

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,3 +2,19 @@
 module.exports = {
   NODE_ENV: '"production"'
 }
+
+/* START :: Customize */
+// https://hyunseob.github.io/2016/07/23/underscore-extend/
+const _ = require('underscore')
+
+_.extend(process.env, module.exports)
+const nuxt  = require('../nuxt.config.js')
+const nuxtEnvEscaped = _.mapObject(nuxt.env, (v) => {
+  if(_.isString(v))
+    return `"${v}"`
+  return v
+})
+console.log(nuxtEnvEscaped)
+
+_.extend(module.exports, nuxtEnvEscaped)
+/* END */
